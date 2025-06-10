054b02c291ff2a873b3412450baa65780dcbc74e
adrien-force <force.adri@gmail.com>
2025-06-10 19:41:17 +0200

    refactor(controller): 🔄 migrate VideoGameController to extend BaseController to fix UserInterface / User type issue

---
+++ src/Controller/BaseController.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Model\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BaseController extends AbstractController
+{
+    #[\Override]
+    protected function getUser(): User
+    {
+        $user = parent::getUser();
+        if ($user instanceof User) {
+            return $user;
+        }
+
+        throw $this->createAccessDeniedException('You must be logged in to access this resource.');
+    }
+
+}
--- src/Controller/VideoGameController.php
+++ src/Controller/VideoGameController.php
@@ -10,14 +10,13 @@ use App\List\VideoGameList\Pagination;
 use App\Model\Entity\Review;
 use App\Model\Entity\VideoGame;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\ValueResolver;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/', name: 'video_games_')]
-final class VideoGameController extends AbstractController
+final class VideoGameController extends BaseController
 {
     #[Route(name: 'list', methods: [Request::METHOD_GET])]
     public function list(
