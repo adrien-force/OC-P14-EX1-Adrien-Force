e73f2dd4475486c6a9959da037c3c20a9f408b14
adrien-force <force.adri@gmail.com>
2025-06-10 20:13:59 +0200

    refactor(fixtures): 🔄 update UserFixtures to use array_fill and foreach for user creation

--- src/Doctrine/DataFixtures/UserFixtures.php
+++ src/Doctrine/DataFixtures/UserFixtures.php
@@ -15,7 +15,10 @@ final class UserFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        $users = \array_fill_callback(0, 10, fn (int $index): User => (new User())
+        $users = array_fill(
+            0,
+            10,
+            fn (int $index): User => (new User())
             ->setUsername($name = $this->faker->userName)
             ->setEmail(sprintf('%s@email.com', $name))
             ->setPlainPassword('password')
@@ -26,7 +29,9 @@ final class UserFixtures extends Fixture
             ->setEmail('testuser@gmail.com')
             ->setPlainPassword('password');
 
-        array_walk($users, [$manager, 'persist']);
+        foreach ($users as $user) {
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
