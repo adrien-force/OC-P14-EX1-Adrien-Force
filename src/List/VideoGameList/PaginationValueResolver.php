824495b4637b1b58a44a1244bdc44295f155483a
adrien-force <force.adri@gmail.com>
2025-06-11 21:05:32 +0200

    refactor(PaginationValueResolver): update return type in tryFromName method to use self and case get on resolve

--- src/List/VideoGameList/PaginationValueResolver.php
+++ src/List/VideoGameList/PaginationValueResolver.php
@@ -14,19 +14,23 @@ use Symfony\Component\HttpKernel\ControllerMetadata\ArgumentMetadata;
 #[AsTargetedValueResolver('pagination')]
 final readonly class PaginationValueResolver implements ValueResolverInterface
 {
-    public function resolve(Request $request, ArgumentMetadata $argument): iterable
+    public function resolve(Request $request, ArgumentMetadata $argument): Pagination
     {
         $argumentType = $argument->getType();
 
         if (Pagination::class !== $argumentType) {
-            return [];
+            throw new \InvalidArgumentException(sprintf(
+                'Expected argument of type "%s", got "%s"',
+                Pagination::class,
+                $argumentType ?? 'null'
+            ));
         }
 
-        return [new Pagination(
+        return new Pagination(
             $request->query->getInt('page', 1),
             $request->query->getInt('limit', 10),
-            Sorting::tryFromName($request->query->get('sorting', '')) ?? Sorting::ReleaseDate,
-            Direction::tryFromName($request->query->get('direction', '')) ?? Direction::Descending,
-        )];
+            Sorting::tryFromName((string)$request->query->get('sorting')) ?? Sorting::ReleaseDate,
+            Direction::tryFromName((string)$request->query->get('direction')) ?? Direction::Ascending
+        );
     }
 }
