241e0ed83db72cc7ac705a91e282b384419076df
adrien-force <force.adri@gmail.com>
2025-06-12 19:52:32 +0200

    refactor(bootstrap): enhance PHP version handling and add type annotation for PHP version variable

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,9 +3,16 @@
 use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__).'/vendor/autoload.php';
-
-if (method_exists(Dotenv::class, 'bootEnv')) {
+/**
+ * @var int<0, 999999> $phpVersion PHPStan type depends on PHP version ¯\_(ツ)_/¯
+ */
+$phpVersion = PHP_VERSION_ID;
+// Load environment variables based on PHP version
+if ($phpVersion >= 70100) { // PHP 7.1 or higher
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+} else {
+    // Fallback for older PHP versions
+    (new Dotenv())->load(dirname(__DIR__).'/.env');
 }
 
 if ($_SERVER['APP_DEBUG']) {
